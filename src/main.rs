b3da8837a26db80a62538ff03faddf28d66de62f
Udit <udit@eskaytech.com>
2023-07-24 13:07:41 +0530

    Include shuttle secrets and surreal db connection

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use axum::{
-    routing::{get, get_service},
+    extract::{Path, Query},
     response::Json,
+    routing::{get, get_service},
     Router,
-    extract::{Query, Path}
 };
 use serde::Deserialize;
+use shuttle_secrets::SecretStore;
+use surrealdb::{
+    engine::remote::ws::{Client, Ws},
+    opt::auth::Root,
+    Surreal,
+};
 use tower_http::services::ServeDir;
 
 pub use self::error::{Error, Result};
@@ -12,13 +18,15 @@ pub use self::error::{Error, Result};
 mod error;
 mod web;
 
+pub static DB: Surreal<Client> = Surreal::init();
+
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
 #[derive(Debug, Deserialize)]
 struct RouteParams {
-    name: Option<String>
+    name: Option<String>,
 }
 
 async fn query_param(Query(params): Query<RouteParams>) -> Json<serde_json::Value> {
@@ -34,16 +42,49 @@ fn routes_test() -> Router {
     return Router::new()
         .route("/", get(hello_world))
         .route("/query-param", get(query_param))
-        .route("/path-param/:name", get(path_param))
+        .route("/path-param/:name", get(path_param));
 }
 
 fn routes_static() -> Router {
-    return Router::new()
-        .nest_service("/", get_service(ServeDir::new("./")))
+    return Router::new().nest_service("/", get_service(ServeDir::new("./")));
+}
+
+async fn connect_db(
+    db_uri: String,
+    db_username: String,
+    db_password: String,
+    db_namespace: String,
+) -> surrealdb::Result<()> {
+    // Connect to the server
+    DB.connect::<Ws>(db_uri).await.expect("Failed to connect");
+
+    DB.signin(Root {
+        username: &db_username,
+        password: &db_password,
+    })
+    .await.expect("Failed to signin");
+
+    DB.use_ns(&db_namespace).use_db(&db_namespace).await.expect("Failed to set namespace");
+
+    Ok(())
 }
 
 #[shuttle_runtime::main]
-async fn axum() -> shuttle_axum::ShuttleAxum {
+async fn axum(#[shuttle_secrets::Secrets] secret_store: SecretStore) -> shuttle_axum::ShuttleAxum {
+    // Connect DB
+    if let (Some(db_uri), Some(db_username), Some(db_password), Some(db_namespace)) = (
+        secret_store.get("DB_URI"),
+        secret_store.get("DB_USERNAME"),
+        secret_store.get("DB_PASSWORD"),
+        secret_store.get("DB_NAMESPACE"),
+    ) {
+        connect_db(db_uri, db_username, db_password, db_namespace)
+            .await
+            .expect("Had some errors running migrations :(");
+    } else {
+        eprintln!("Unable to retrieve all necessary secrets for database connection");
+    }
+
     let router = Router::new()
         .merge(routes_test())
         .merge(web::routes_login::routes())
--- src/web/routes_login.rs
+++ src/web/routes_login.rs
@@ -1,8 +1,9 @@
 use axum::{Json, Router, routing::post};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use serde_json::{Value, json};
+use surrealdb::sql::Thing;
 
-use crate::{Error, Result};
+use crate::{Result, DB};
 
 #[derive(Debug, Deserialize)]
 struct LoginPayload {
@@ -10,21 +11,41 @@ struct LoginPayload {
   pwd: String
 }
 
+#[derive(Debug, Serialize)]
+struct Name<'a> {
+    first: &'a str,
+    last: &'a str,
+}
+
+#[derive(Debug, Serialize)]
+struct Person<'a> {
+    title: &'a str,
+    name: Name<'a>,
+    marketing: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Record {
+    #[allow(dead_code)]
+    id: Thing,
+    title: String,
+    marketing: bool
+}
+
 pub fn routes() -> Router {
   Router::new()
     .route("/api/login", post(api_login))
 }
 
-async fn api_login(payload: Json<LoginPayload>) -> Result<Json<Value>> {
+async fn api_login(_payload: Json<LoginPayload>) -> Result<Json<Value>> {
   println!("Do API Login");
 
-  if payload.username != "demo" || payload.pwd != "123" {
-      return Err(Error::LoginFail);
-  }
+  let people: Vec<Record> = DB.select("person").await.expect("Cant get list");
 
   let body = Json(json!({
     "result": {
-      "success": true
+      "success": true,
+      "people": people
     }
   }));
 
